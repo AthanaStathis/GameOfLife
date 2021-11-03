c8348c8295e554e977fc8de4dae12367aed4a676
Athanasios <athana.stathis@gmail.com>
2021-11-03 19:44:08 +0100

    isAlive failed test

--- src/test/java/TDDtests.java
+++ src/test/java/TDDtests.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import javax.swing.text.html.AccessibleHTML;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TDDtests {
+
+    @Test
+    void isAlive() {
+        assertEquals(board.getCell(0).isAlive());
+    }
+
 }
